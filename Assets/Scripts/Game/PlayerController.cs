174522ae78023e74e029dcd889e1b3350c412601
John Smith <john.ayudante@tecsup.edu.pe>
2025-07-17 18:15:45 -0500

    Player movement plus camera direction performed

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -1,24 +1,62 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
     private CharacterController characterController;
+
+    [Header("Movimiento del jugador")]
+    private Vector2 inputMovement;
+    private Vector3 movement;
+
+    [SerializeField] private float velocity = 5f;
+    [SerializeField] private float rotationSpeed = 10f;
+
+    [Header("Referencia a la cámara")]
+    [SerializeField] private Transform cameraTransform;
+
     private void OnEnable()
     {
-
+        InputReader.OnMove += Movement;
     }
+
     private void OnDisable()
     {
-
+        InputReader.OnMove -= Movement;
     }
+
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
     }
+
+    private void Start()
+    {
+        if (cameraTransform == null && Camera.main != null)
+            cameraTransform = Camera.main.transform;
+    }
+
     private void Update()
     {
-        
+        Vector3 camForward = cameraTransform.forward;
+        Vector3 camRight = cameraTransform.right;
+        camForward.y = 0;
+        camRight.y = 0;
+        camForward.Normalize();
+        camRight.Normalize();
+
+        movement = camRight * inputMovement.x + camForward * inputMovement.y;
+
+        characterController.Move(movement * velocity * Time.deltaTime);
+
+        if (movement != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+    public void Movement(Vector2 move)
+    {
+        inputMovement = move;
     }
 }
--- Assets/Scripts/Input/InputReader.cs
+++ Assets/Scripts/Input/InputReader.cs
@@ -1,6 +1,13 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputReader : MonoBehaviour
 {
-    
+    public static event Action<Vector2> OnMove;
+
+    public void Movement(InputAction.CallbackContext context)
+    {
+        OnMove?.Invoke(context.ReadValue<Vector2>());
+    }
 }
