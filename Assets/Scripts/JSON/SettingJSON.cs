25489afe122a3809a35b9529a82f755a4da7e97f
Carlos Saire <carlos.saire@tecsup.edu.pe>
2025-07-20 01:26:33 -0500

    Desactive Show Splash Screen Unity

--- Assets/Scripts/JSON/SettingJSON.cs
+++ Assets/Scripts/JSON/SettingJSON.cs
@@ -5,7 +5,14 @@ public class SettingJSON
     [Range(0, 1)] public float master;
     [Range(0, 1)] public float music;
     [Range(0, 1)] public float sfx;
-    public float sensibility;
+    [Range(0, 1)] public float sensibility;
+    public SettingJSON()
+    {
+        this.master = 1;
+        this.music = 1;
+        this.sfx = 1;
+        this.sensibility = 1;
+    }
     public SettingJSON(float master, float music, float sfx, float sensibility)
     {
         this.master = master;
